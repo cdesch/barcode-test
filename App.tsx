7f9d7ccb1ac5baa1081f06377860be451a097fc3
Chris Desch <cdesch@gmail.com>
2019-12-17 11:02:24 -0500

    adding barcode component placeholder

--- App.tsx
+++ App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import BarcodeComponent from './src/BarcodeComponent';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
+      <BarcodeComponent name="this name" />
     </View>
   );
 }
